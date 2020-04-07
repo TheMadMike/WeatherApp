b1fd3296bc8d24f5ff1db18b1929b377de9d4298
MadRussianHacker <m.gibas2407@gmail.com>
2020-04-07 22:11:26 +0200

    Add geolocalization

--- weather.js
+++ weather.js
@@ -1,26 +1,50 @@
+document.onload = askForGeolocation();
+
 const cityNameInput = document.getElementById('cityName');
+const apiKey = '2460a70aa4ddf323987fb22827cc06e7';
 
 function showWeather() {
     let cityName = cityNameInput.value;
     getWeatherForCity(cityName);
 }
 
+function askForGeolocation() {
+    if(!navigator.geolocation) {
+        console.log('Geolocation not supported.');
+    } 
+    
+    else {
+        navigator.geolocation.getCurrentPosition((position) => {
+
+          getWeatherForCoordinates(position.coords.latitude, position.coords.longitude);
+
+        }, () => { 
+            console.log("Unable to retrieve user's location"); 
+        });
+    }
+}
+
 function getWeatherForCity(cityName) {
     let request = new XMLHttpRequest();
-
-    const apiKey = '2460a70aa4ddf323987fb22827cc06e7';
-    request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+apiKey, )
+    request.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`);
 
     request.onload = function() {
         let data = JSON.parse(this.response);
+        displayData(data);
+    };
 
-        //check if http status code == OK
-        if(data.cod == 200) {
-            displayData(data);
-        } else {
-            alert(data.message);
-        }
-    }
+    request.send();
+}
+
+function getWeatherForCoordinates(latitude, longitude) {
+    let request = new XMLHttpRequest();
+    request.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
+
+    request.onload = function() {
+        let data = JSON.parse(this.response);
+        displayData(data);
+        cityNameInput.value = data.name;
+    };
 
     request.send();
 }
@@ -30,8 +54,14 @@ function toCelsius(kelvin) {
 }
 
 function displayData(data) {
-    document.getElementById('desc').innerHTML = `${data.weather[0].main} (${data.weather[0].description})`;
-    document.getElementById('pressure').innerHTML = `Pressure: ${data.main.pressure} hPa`;
-    document.getElementById('temp').innerHTML = `Temperature: ${Math.round(toCelsius(data.main.temp))} &deg;C`;
-    document.getElementById('humidity').innerHTML = `Humidity: ${data.main.humidity} %`;
+    if(data.cod == 200) {
+        document.getElementById('desc').innerHTML = `${data.weather[0].main} (${data.weather[0].description})`;
+        document.getElementById('pressure').innerHTML = `Pressure: ${data.main.pressure} hPa`;
+        document.getElementById('temp').innerHTML = `Temperature: ${Math.round(toCelsius(data.main.temp))} &deg;C`;
+        document.getElementById('humidity').innerHTML = `Humidity: ${data.main.humidity} %`;
+    }
+
+    else {
+        alert(data.message);
+    }
 }
