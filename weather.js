328178cf8d2336bee0dd95c9a9edfebc860de2e6
MadRussianHacker <m.gibas2407@gmail.com>
2020-04-07 20:13:28 +0200

    Create weather.js, add simple code to interact with weather API

---
+++ weather.js
@@ -0,0 +1,30 @@
+const cityNameInput = document.getElementById('cityName');
+const currentWeatherCtx = document.getElementById('current');
+
+function showWeather() {
+    let cityName = cityNameInput.value;
+    getWeatherForCity(cityName);
+}
+
+function getWeatherForCity(cityName) {
+    let request = new XMLHttpRequest();
+
+    const apiKey = '2460a70aa4ddf323987fb22827cc06e7';
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+apiKey, )
+
+    request.onload = function() {
+        let data = JSON.parse(this.response);
+        if(data.cod == 200) {
+            currentWeatherCtx.innerHTML = `${data.weather[0].main} (${data.weather[0].description}) <br>` ;
+            currentWeatherCtx.innerHTML += `temp: ${Math.round(toCelsius(data.main.temp))} &deg;C`;
+        } else {
+            alert(data.message);
+        }
+    }
+
+    request.send();
+}
+
+function toCelsius(kelvin) {
+    return (kelvin - 273);
+}
